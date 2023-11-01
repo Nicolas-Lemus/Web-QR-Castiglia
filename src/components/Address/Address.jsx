38acbe951615233071af6967c61590100c125933
Nicolas Lemus <nicolaswaldemarlemuscurbelo@gmail.com>
2023-11-01 19:51:08 -0300

    cambios en iconos de navegacion y fondo

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import './Address.css'
 import Buttons from "../Buttons/Buttons"
-import WhatsApp from "../../images/iconos-nuevos/icons8-whatsapp-96.webp"
-import Facebook from "../../images/iconos-nuevos/icons8-facebook-96 (1).webp"
-import Web from "../../images/iconos-nuevos/icons8-web-96 (1) (1).webp"
-import Instagram from '../../images/iconos-nuevos/icons8-instagram-96.webp'
-import Llamadas from '../../images/iconos-nuevos/icons8-llamada-96.webp'
-import Email from '../../images/iconos-nuevos/icons8-correo-electrónico-96 (1).webp'
+import WhatsApp from "../../images/iconos/icons8-whatsapp-96.webp"
+import Facebook from "../../images/iconos/icons8-facebook-96.webp"
+import Web from "../../images/iconos/icons8-web-94.webp"
+import Instagram from '../../images/iconos/icons8-instagram-96.webp'
+import Llamadas from '../../images/iconos/icons8-llamada-100.webp'
+import Email from '../../images/iconos/icons8-correo-electrónico-96.webp'
 
 const Address = () => {
 
 const addressWeb ="#"
 const addressInstagram = "https://www.instagram.com/castigliaok/"
-const addressFacebook = "https://www.facebook.com/castiglia.nuevasraices.1"
+const addressFacebook = "https://www.facebook.com/castigliaok/"
 const addressWhatsApp = "https://wa.me/542634876086?text=Hola%20Castiglia!%20Escribo%20para%20consultarles..."
 const addressTel = "tel:02634423186"
 const addressEmail = "mailto:ventascastiglia@hotmail.com?subject=Hola Castiglia! Escribo para consultarles...";
